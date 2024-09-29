837717fe13950e888a31b2f627634dd2965637c9
Adhish Bangera <adhish.bangera18@gmail.com>
2024-09-29 12:40:00 +0530

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,19 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt=>{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors(opt=>{
-    opt.AddPolicy("CorsPolicy",policy=>{
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-}); 
 
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,7 +36,7 @@ var services = scope.ServiceProvider;
 
 try
 {
-    var context= services.GetRequiredService<DataContext>();
+    var context = services.GetRequiredService<DataContext>();
     await context.Database.MigrateAsync();
     await Seed.SeedData(context);
 
@@ -49,7 +44,7 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error occured during migration");
+    logger.LogError(ex, "An error occured during migration");
 }
 
 app.Run();
